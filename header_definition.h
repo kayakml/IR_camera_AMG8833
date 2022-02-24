c51c3e88e8e40f54f47b83624ae446e2e9e71989
kayakml <30715872+kayakml@users.noreply.github.com>
2022-02-24 11:53:08 +0300

    Update header_definition.h

--- header_definition.h
+++ header_definition.h
@@ -1,1 +1,22 @@
-dd
+//location -->> mylibraries
+
+#ifndef HEADER_DEFINITION_H
+#define HEADER_DEFINITION_H
+
+//#define Bedheater_LCD
+////#define my_OV7670
+//#define pic24f_deneme
+//#define dspic33_deneme
+//#define AMG8833_1
+#define IR_cam_PIC24f
+//#define pic24f_deneme_2
+//#define pic24f_lcd_st7789
+//#define BlueTooth
+//#define MAFS
+//#define DATAACQ
+//#define dspic33E_ADC
+//#define auo_lcd
+//#define _93C46_eprom
+
+
+#endif
